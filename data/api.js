a921d8370e28d8a0e52eea904127a9b0dacb2f17
St Fu <it@s-fuhrmann.de>
2018-04-19 02:49:37 +0200

    API+Grid
    API works for now
    Started using Grid(might hate myself)

--- data/api.js
+++ data/api.js
@@ -5,14 +5,20 @@ function rndQuote() {
     },
     url: "https://random-quote-generator.herokuapp.com/api/quotes/",
     success: function(r){
-      console.log(r);
       var rndQuoteObj = r[Math.floor(Math.random()*r.length)];
-      console.log(rndQuoteObj);
+      //Can Jquery do cool animation or sth?
+      
       $("#text").text(rndQuoteObj.quote);
       $("#author").text(rndQuoteObj.author);
+      $(".tweet").attr("href", "https://twitter.com/intent/tweet?hashtags=DailyWisdom&text=My%20daily%20wisdom:" + rndQuoteObj.quote + "%20" + rndQuoteObj.author);
+      
+      //https://dev.twitter.com/web/tweet-button/parameters
+     
+      
       
     },
     error: function(a, b, c){
+      console.log(a);
       console.log(b);
       console.log(c);
     }
@@ -23,6 +29,10 @@ function rndQuote() {
 $(document).ready(function() {
   rndQuote();
   $('#new-quote').on('click', rndQuote);})
+
+
+ 
+  
 /**
  * https://quotes.rest/#!/quote/get_quote_random
  * https://theysaidso.com/api/
